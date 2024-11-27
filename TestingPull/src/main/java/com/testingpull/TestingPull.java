d3fd31cc352c1d29188cf73bcb0f4c81517ac09c
Banica Dragos <50632140+DragosXP@users.noreply.github.com>
2024-11-27 08:54:54 +0200

    Added Project

---
+++ TestingPull/src/main/java/com/testingpull/TestingPull.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testingpull;
+
+/**
+ *
+ * @author GrayHat667
+ */
+public class TestingPull {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
